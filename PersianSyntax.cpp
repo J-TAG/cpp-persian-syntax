1756a66d471b895874cd6d46fb6cb52d403e770b
Hesam Gholami <hgholami@infotech-co.net>
2017-03-28 15:27:45 +0430

    Add v1.1 files

---
+++ PersianSyntax.cpp
@@ -0,0 +1,78 @@
+﻿// PersianSyntax.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+// Load Persian Syntax Header File
+#include "PersianSyntax.h"
+
+#include "iostream"
+
+استفاده_از_فضای_اسمی std;
+
+کلاس MyClass
+{
+عمومی:
+	MyClass() {};
+	مجازی ~MyClass() {};
+	
+محافظت_شده:
+
+
+خصوصی:
+
+};
+
+عدد اصلی()
+{
+	عدد x برابربا 0;
+	
+	cout << "Please enter a number: ";
+    cin >> x;
+
+	اگر(x بزرگترمساوی 10)
+	{
+		cout << "You Enter a number greater than or equal 10.\n\n";
+	}
+	دیگر
+	{
+		cout << "You Enter a number lower than 10.\n\n";
+	}
+
+	برای(عدد i برابربا 0; i بزرگتراز 10; i++ )
+	{
+		cout << i << endl;
+	}
+
+	عدد y = 0;
+
+
+	برای_همیشه
+	{
+		cout << "Please enter 5 to exit loop: ";
+		cin >> y;
+
+		اگر(y مساوی 5)
+		{
+			بشکن;
+		}
+	}
+
+	MyClass* pMyObj = جدید MyClass;
+	MyClass myObj2;
+
+	اگر(pMyObj مساوی تهی)
+	{
+		cout << "First Object is Null.\n";
+	}
+	دیگر اگر(اندازه_ی(pMyObj) مساوی اندازه_ی(myObj2))
+	{
+		cout << "Objects size are equal.\n";
+	}
+
+	حذف pMyObj;
+
+	
+	برگشتی 0;
+}
+
---
+++ PersianSyntax.h
@@ -0,0 +1,56 @@
+﻿/* Persian Syntax v1.1 | By Hesam Gholami. http://hesam.org */
+
+#define				اگر						if
+#define				دیگر					else
+#define				برای					for
+#define				برای_همیشه				for(;;)
+#define				برای_هر					for each
+#define				انجام_بده				do
+#define				تاوقتی					while
+#define				تعویض					switch
+#define				سویچ					switch
+#define				کلاس						class
+#define				ساختار					struct
+#define				شمارشی					enum
+#define				فضای_اسمی				namespace
+#define				تعریف_نوع_داده			typedef
+#define				قالب					template
+#define				نام_قالب				typename
+#define				پیوند					union
+#define				اندازه_ی				sizeof
+#define				خصوصی					private
+#define				محافظت_شده				protected
+#define				عمومی					public
+#define				مجازی					virtual
+#define				ثابت					static
+#define				دوست					friend
+#define				حذف						delete
+#define				جدید					new
+#define				بشکن					break
+#define				برگشتی					return
+#define				ادامه					continue
+#define				خودکار					auto
+#define				استفاده_از				using
+#define				استفاده_از_فضای_اسمی	using namespace
+#define				بلاتصدی					void
+#define				بدون_بازگشت			void
+#define				بدون_علامت				unsigned
+#define				علامت_دار				signed
+#define				عدد_دراز				long
+#define				عدد_اعشاری				double
+#define				عدد_شناور				float
+#define				عدد						int
+#define				حرف						char
+#define				برابربا				=
+#define				نامساوی				!=
+#define				مساوی					==
+#define				بزرگتراز				>
+#define				بزرگترمساوی			>=
+#define				کوچکتراز				<
+#define				کوچکترمساوی			<=
+#define				نیست					!
+#define				اصلی					main
+#define				تهی						NULL
+
+
+/* End Persian Syntax */
